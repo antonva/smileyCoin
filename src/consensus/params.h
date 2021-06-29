e5c4c9363ac148413c6f32247b02303025312259
Anton Vilhelm Ásgeirsson <antonva@users.noreply.github.com>
2021-06-29 20:03:21 +0000

    [smly] Fix consensus params for first target timespan change

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -58,8 +58,11 @@ struct Params {
     int BIP65Height;
     /** Block height at which BIP66 becomes active */
     int BIP66Height;
-    /** Block height at which Premine is over*/
+    /** [smly] Block height at which Premine is over*/
     int PremineEndHeight;
+    /** [smly] Block height when first target timespan change was made and original timespan*/
+    int FirstTimespanChangeHeight;
+    int64_t nPowOriginalTargetTimespan;
     /**
      * Minimum blocks including miner confirmation of the total of 2016 blocks in a retargeting period,
      * (nPowTargetTimespan / nPowTargetSpacing) which is also used for BIP9 deployments.
